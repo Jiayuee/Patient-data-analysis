a1d56b93466ad0f456df9fa8a84787185ac6e1b2
Jiayuee <zhangjiayuelily@gmail.com>
2018-08-01 13:51:33 +0800

    t_test controling age groups

--- t_test.py
+++ t_test.py
@@ -4,7 +4,12 @@ from scipy.stats import ttest_ind
 ip = 'edited_data.h5'
 
 df = pd.read_hdf(ip, 'LDLC_Comparision','r+')
-cat1 = df[df['LDL Record'] == 'Baseline']
-cat2 = df[df['LDL Record'] == 'Latest']
 
-print(ttest_ind(cat1['LDL'],cat2['LDL']))
+age_groups = ['40-60 years old','60-80 years old','80-100 years old','20-40 years old']
+for a in age_groups:
+    idx1 = df['LDL Record'] == 'Baseline'
+    idx2 = df['LDL Record'] == 'Latest'
+    idx3 = df['Age Group'] == a
+    cat1 = df[idx1&idx3]
+    cat2 = df[idx2&idx3]
+    print(a,ttest_ind(cat1['LDL'],cat2['LDL']))
